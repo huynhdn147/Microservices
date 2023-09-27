1526cda7aae41b421d2c41199f9cf45dec8b4cd1
huynh.nguyendinh <huynh.nguyendinh@fssc.com.vn>
2023-09-28 01:01:57 +0700

    add mesageBus

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private string connectionString = "Endpoint=sb://mangowebservicebushuynhdn.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=OqbcmKg+cwzVFrJNyIb65OLKayBaFdyDY+ASbF+ros0=";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            var sender =  client.CreateSender(topic_queue_Name);
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            ServiceBusMessage finalMessage = new ServiceBusMessage(
+                Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
+}
